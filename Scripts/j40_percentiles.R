93c0cd44d5fcff790f04b36a103032ca8f234b8c
Dennis Duffy <dennis.duffy@state.mn.us>
2024-07-22 15:31:41 -0500

    combined j40 and city boundaries

---
+++ Scripts/j40_percentiles.R
@@ -0,0 +1,87 @@
+library(sf)
+library(tidyverse)
+
+sf::sf_use_s2(FALSE)
+
+mn <- st_read("j40_spatial/j40_spatial.gdb", layer = "usa", 
+               query = "
+    select
+      GEOID10,
+      CF as county_name, 
+        DF_PFS + 
+        AF_PFS + 
+        HDF_PFS +
+        DSF_PFS + 
+        EBF_PFS +
+        EALR_PFS +
+        EBLR_PFS + 
+        EPLR_PFS + 
+        HBF_PFS + 
+        LLEF_PFS +
+        LIF_PFS + 
+        LMI_PFS + 
+        PM25F_PFS + 
+        P100_PFS +
+        P200_I_PFS +
+        LPF_PFS + 
+        KP_PFS +
+        NPL_PFS + 
+        RMP_PFS + 
+        TSDF_PFS + 
+        TF_PFS + 
+        UF_PFS + 
+        WF_PFS +
+        UST_PFS + 
+        TD_PFS + 
+        FLD_PFS + 
+        WFR_PFS +
+        FUDS_ET +
+        AML_ET +
+      IS_PFS as total_percentiles,  
+      HRS_ET as redlining, 
+      SN_C as dac,
+      FPL200S as low_income
+    from usa
+    where SF = 'Minnesota'
+                 ") |> 
+  mutate(redlining = case_when(
+    redlining == "1" ~ 1,
+    TRUE ~ 0
+  ), 
+  total_percentiles = total_percentiles + redlining, 
+  rank = cume_dist(total_percentiles), 
+  county_name = str_replace(county_name, " County", ""), 
+  dac = ifelse(dac == 1, "Disadvantaged", "Not Disadvantaged")) |> 
+  select(-c(redlining))
+
+#boundary database retrieved from https://gisdata.mn.gov/dataset/bdry-mn-city-township-unorg
+
+boundaries <- st_read("town_boundaries/bdry_mn_city_township_unorg.gdb", layer = "city_township_unorg",
+                      query = "
+                      select FEATURE_NAME as city,
+                      COUNTY_NAME as county
+                      from city_township_unorg
+                      where CTU_CLASS = 'CITY'
+                      ") |>
+  mutate(county = case_when(
+    county == "Saint Louis" ~ "St. Louis",
+    TRUE ~ county
+  )) |> 
+  st_transform("EPSG:4326") |>
+  st_cast("MULTIPOLYGON")
+
+boundary_tracts <- boundaries |> 
+  st_join(mn, join = st_intersects) |> 
+  group_by(city) |> 
+  mutate(multi_county_flag = length(county) == TRUE, 
+         drop_flag = case_when(
+           multi_county_flag == FALSE & county != county_name ~ TRUE, 
+           TRUE ~ FALSE
+         )) |> 
+  filter(drop_flag == FALSE) |> 
+  tibble() |> 
+  select(city, GEOID10)
+
+
+
+
